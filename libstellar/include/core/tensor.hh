db67f3062addb49123a50f97e4e6b2db71063a74
Yassine <yassine.tlich@epita.fr>
2026-04-19 14:12:32 +0200

    feat(license): add of Apache 2.0 license. README adjusted

--- libstellar/include/core/tensor.hh
+++ libstellar/include/core/tensor.hh
@@ -1,3 +1,17 @@
+// Copyright 2026 Yassine TLICH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef TENSOR_H
 #define TENSOR_H
 
@@ -74,7 +88,8 @@ namespace stellar::core
          * @brief Get the element of the row-th row and col-th column.
          * Indexes are zero-based.
          */
-        [[nodiscard]] scalar_t get(const unsigned int row, const unsigned int col) const
+        [[nodiscard]] scalar_t get(const unsigned int row,
+                                   const unsigned int col) const
         {
             assert(row < size_ && col < length_);
             return elements_[row * length_ + col];
@@ -87,7 +102,8 @@ namespace stellar::core
          * @brief Set a new element 'val' of the row-th row and col-th column.
          * Indexes are zero-based.
          */
-        void set(const unsigned int row, const unsigned int col, const scalar_t val)
+        void
+        set(const unsigned int row, const unsigned int col, const scalar_t val)
         {
             assert(row < size_ && col < length_);
             elements_[row * length_ + col] = val;
